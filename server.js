2dbe733efdbe83981f6bad57c1ca848e30f96eb3
Abhishek Ahirwar <ahirwarabhi01@gmail.com>
2025-07-12 17:37:44 +0530

    added build folder

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 //dot config
 dotenv.config();
 
@@ -26,13 +27,21 @@ app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
+// static folder
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// static routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 //port
 const PORT = process.env.PORT || 8080;
 
 //listen
 app.listen(PORT, () => {
   console.log(
-    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+    `Node Server Running In ${process.env.DEV_MODE} Mode On Port ${process.env.PORT}`
       .bgBlue.white
   );
 });
